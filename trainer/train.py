2554c08e50a2d07daf12a437c6662e78a0475012
Chrisantus Eze <ezechris30@gmail.com>
2023-09-26 05:57:45 -0500

    update

--- trainer/train.py
+++ trainer/train.py
@@ -141,8 +141,8 @@ def train_fcn(args):
     val_dataset = HeightMapDataset(args, val_ids)
 
     # note: the batch size is 1
-    data_loader_train = data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=4, pin_memeory=True, shuffle=True)
-    data_loader_val = data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=4, pin_memeory=True)
+    data_loader_train = data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=4, pin_memory=True, shuffle=True)
+    data_loader_val = data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=4, pin_memory=True)
 
     data_loaders = {'train': data_loader_train, 'val': data_loader_val}
     logging.info('{} training data, {} validation data'.format(len(train_ids), len(val_ids)))
