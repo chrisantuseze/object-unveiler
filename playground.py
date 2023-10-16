b8b005d9b06637b0ba547ec3bc9436ad551982da
Chrisantus Eze <ezechris30@gmail.com>
2023-10-16 00:51:12 -0500

    fix mem issue

--- playground.py
+++ playground.py
@@ -31,10 +31,36 @@ import os
 #             print(filename)
 
 
-directory = "/Users/chrisantuseze/Research/robot-learning/ppg-datasets/ppg-dataset/"
-for root, dirs, files in os.walk(directory, topdown=False):
-    for dir in dirs:
-        dir_path = os.path.join(root, dir)
-        if not os.listdir(dir_path):
-            os.rmdir(dir_path)
-            print(f"Removed empty directory: {dir_path}")
+# directory = "/Users/chrisantuseze/Research/robot-learning/ppg-datasets/ppg-dataset/"
+# for root, dirs, files in os.walk(directory, topdown=False):
+#     for dir in dirs:
+#         dir_path = os.path.join(root, dir)
+#         if not os.listdir(dir_path):
+#             os.rmdir(dir_path)
+#             print(f"Removed empty directory: {dir_path}")
+
+
+
+import numpy as np
+
+# Sample list of tuples, each containing three tensors
+tensor_tuple_list = [(np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9])),
+                    (np.array([1, 2, 3, 4]), np.array([5, 6, 7]), np.array([8, 9, 10]))]
+
+# Get the size of the tensors in the first tuple to compare with others
+first_tuple_sizes = [len(tensor) for tensor in tensor_tuple_list[0]]
+
+# Initialize a list to store the indices of tuples with different sizes
+indices_of_different_sizes = []
+
+for i, tuple_elem in enumerate(tensor_tuple_list):
+    if not all(len(tensor) == size for tensor, size in zip(tuple_elem, first_tuple_sizes)):
+        indices_of_different_sizes.append(i)
+
+if not indices_of_different_sizes:
+    print("All tuples have tensors with equal sizes.")
+else:
+    print("Tuples with different-sized tensors:")
+    for idx in indices_of_different_sizes:
+        print(f"Tuple at index {idx}: {tensor_tuple_list[idx]}")
+
