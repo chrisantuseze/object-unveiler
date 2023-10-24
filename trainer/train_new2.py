d6afababcda5b602935784b033847a54fff71bc0
Chrisantus Eze <ezechris30@gmail.com>
2023-10-24 12:21:51 -0500

    update

--- trainer/train_new2.py
+++ trainer/train_new2.py
@@ -76,7 +76,7 @@ def train_fcn_net(args):
             loss = criterion(pred, y)
             loss = torch.sum(loss)
 
-            logging.info(f"train step [{step}/{len(data_loader_train)}]\t Loss: {loss.detach().cpu().numpy()}")
+            # logging.info(f"train step [{step}/{len(data_loader_train)}]\t Loss: {loss.detach().cpu().numpy()}")
 
             optimizer.zero_grad()
             loss.backward()
