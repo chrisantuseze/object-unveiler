3393fe7b7bbcba729de249b6bcdedd1911bd0d59
Chrisantus Eze <ezechris30@gmail.com>
2023-11-02 12:38:28 -0500

    training using ppg-ou-dataset on tacc

--- playground.py
+++ playground.py
@@ -4,7 +4,7 @@ import zipfile
 import pickle
 import os
 
-with zipfile.ZipFile("ppg-ou-dataset.zip", 'r') as zip_ref:
+with zipfile.ZipFile("ppg-ou-datasets.zip", 'r') as zip_ref:
     zip_ref.extractall("ppg-ou-dataset")
 
 
