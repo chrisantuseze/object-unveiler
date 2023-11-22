0ef8efe8fdf1a978f6f85ab1d180f0cad1f08f73
Chrisantus Eze <ezechris30@gmail.com>
2023-11-22 15:45:20 -0600

    training on ppg-ou-dataset

--- trainer/memory.py
+++ trainer/memory.py
@@ -99,12 +99,13 @@ class ReplayBuffer:
 
     def load(self, dir_ids, idx):
         try:
+            print(os.path.join(self.save_dir, dir_ids[idx], 'heightmap.exr'))
             heightmap = cv2.imread(os.path.join(self.save_dir, dir_ids[idx], 'heightmap.exr'), -1)
             target_mask = cv2.imread(os.path.join(self.save_dir, dir_ids[idx], 'target_mask.png'), -1)
             action = pickle.load(open(os.path.join(self.save_dir, dir_ids[idx], 'action'), 'rb'))
         except:
             heightmap = cv2.imread(os.path.join(self.save_dir, dir_ids[idx+1], 'heightmap.exr'), -1)
-            target_mask = cv2.imread(os.path.join(self.save_dir, dir_ids[idx], 'target_mask.png'), -1)
+            target_mask = cv2.imread(os.path.join(self.save_dir, dir_ids[idx+1], 'target_mask.png'), -1)
             action = pickle.load(open(os.path.join(self.save_dir, dir_ids[idx+1], 'action'), 'rb'))
 
         return heightmap, target_mask, action
