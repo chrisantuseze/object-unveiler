fixed issue with heightmap ds broadcast size

datasets/heightmap_dataset.py
@@ -304,11 +304,11 @@ class HeightMapDataset(data.Dataset):
 
 
         N, C, H, W = padded_obj_masks.shape
+        object_masks = np.array(object_masks)
         if N < self.args.num_patches:
             new_padded_obj_masks = np.zeros((self.args.num_patches, C, H, W), dtype=padded_obj_masks.dtype)
             new_padded_obj_masks[:padded_obj_masks.shape[0], :, :, :] = padded_obj_masks
 
-            object_masks = np.array(object_masks)
             N, H, W = object_masks.shape
             new_obj_masks = np.zeros((self.args.num_patches, H, W), dtype=object_masks.dtype)
             new_obj_masks[:object_masks.shape[0], :, :] = object_masks