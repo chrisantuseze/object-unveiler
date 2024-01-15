86b2bd9257069e14fe69de28963fe88711e253c9
Chrisantus Eze <ezechris30@gmail.com>
2024-01-14 20:22:46 -0600

    training with new ds

--- trainer/train_new2.py
+++ trainer/train_new2.py
@@ -27,10 +27,10 @@ def train_fcn_net(args):
     transition_dirs = os.listdir(args.dataset_dir)
     
     for file_ in transition_dirs:
-        # if not file_.startswith("episode"):
-        #     transition_dirs.remove(file_)
-        if not file_.startswith("transition"):
+        if not file_.startswith("episode"):
             transition_dirs.remove(file_)
+        # if not file_.startswith("transition"):
+        #     transition_dirs.remove(file_)
     
     # split data to training/validation
     random.seed(0)
