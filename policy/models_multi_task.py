1aa819c173d832fc2a09b29d10c1b92888a73b6f
Chrisantus Eze <ezechris30@gmail.com>
2024-03-03 12:32:32 -0600

    training only grasping

--- policy/models_multi_task.py
+++ policy/models_multi_task.py
@@ -530,7 +530,7 @@ class ResFCN(nn.Module):
         # out_probs = torch.rand(B, self.args.sequence_length, C, H, W)
         # out_probs = Variable(out_probs, requires_grad=True).to(self.device)
 
-        return None, out_probs #scores, out_probs
+        return scores, out_probs
     
 
 def print_msg(B, *args):
