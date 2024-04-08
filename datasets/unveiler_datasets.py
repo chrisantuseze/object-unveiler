3f9617aeaf5bf5f1129396957a0a0233cd4ec296
Chrisantus Eze <ezechris30@gmail.com>
2024-04-08 02:41:45 -0500

    update

--- datasets/unveiler_datasets.py
+++ datasets/unveiler_datasets.py
@@ -168,7 +168,6 @@ class UnveilerDataset(data.Dataset):
         N, C, H, W = _processed_obj_masks.shape
         object_masks = np.array(object_masks)
         bboxes = np.array(bboxes)
-        print("bbox.shape", bboxes.shape)
         if N < self.args.num_patches:
             processed_obj_masks = np.zeros((self.args.num_patches, C, H, W), dtype=_processed_obj_masks.dtype)
             processed_obj_masks[:_processed_obj_masks.shape[0], :, :, :] = _processed_obj_masks
@@ -179,12 +178,10 @@ class UnveilerDataset(data.Dataset):
 
             num_zeros = self.args.num_patches - bboxes.shape[0]
             bbox = np.pad(bboxes, pad_width=((0, num_zeros), (0, 0)), mode='constant')
-            print("1 bbox.shape", bbox.shape)
         else:
             processed_obj_masks = _processed_obj_masks[:self.args.num_patches]
             obj_masks = object_masks[:self.args.num_patches]
             bbox = bboxes[:self.args.num_patches]
-            print("2 bbox.shape", bbox.shape)
 
         # optimal_nodes = general_utils.apply_softmax(optimal_nodes)
         # if len(optimal_nodes) < self.args.num_patches:
