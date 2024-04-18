d2d7968be5054b5924bacfcad4c7fccc2730f518
Chrisantus Eze <ezechris30@gmail.com>
2024-04-18 08:59:23 -0500

    training target model

--- trainer/train_new2.py
+++ trainer/train_new2.py
@@ -35,6 +35,8 @@ def train_fcn_net(args):
     random.seed(0)
     random.shuffle(transition_dirs)
 
+    transition_dirs = transition_dirs[:8000]
+
     split_index = int(args.split_ratio * len(transition_dirs))
     train_ids = transition_dirs[:split_index]
     val_ids = transition_dirs[split_index:]
