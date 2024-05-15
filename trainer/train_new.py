579b6cf811c723c7438c6de59200fe3984cf125d
Chrisantus Eze <ezechris30@gmail.com>
2024-05-15 14:04:41 -0500

    fixing cuda issue

--- trainer/train_new.py
+++ trainer/train_new.py
@@ -280,6 +280,7 @@ def train_fcn_net(args):
                 rotations
             )
 
+            print(obstacle_gt.long())
             loss = obstacle_criterion(obstacle_pred, obstacle_gt.long())
             loss = torch.sum(loss)
 
