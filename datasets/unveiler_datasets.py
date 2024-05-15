21b582129244a933aa24234bd7804098bde57ab1
Chrisantus Eze <ezechris30@gmail.com>
2024-05-15 15:33:19 -0500

    fixing cuda issue

--- datasets/unveiler_datasets.py
+++ datasets/unveiler_datasets.py
@@ -117,11 +117,13 @@ class UnveilerDataset(data.Dataset):
         labels, rot_ids, obstacle_ids = self.pad_labels_and_rot(len(episode_data), processed_heightmap, labels, rot_ids, obstacle_ids)
         obstacle_ids = obstacle_ids[0] # Refer to notebook for why I did this.
 
+        objects_to_remove = np.argmax(objects_to_remove)
+
         # pad object masks
         processed_obj_masks, obj_masks, bbox = self.pad_object_masks_and_nodes(_processed_obj_masks, object_masks, bboxes)
 
         return processed_heightmap, processed_target_mask, processed_obj_masks\
-             , processed_scene_mask, rot_ids, labels, obstacle_ids, bbox
+             , processed_scene_mask, rot_ids, labels, objects_to_remove, bbox
 
         # return processed_heightmap, processed_target_mask, processed_obj_masks\
         #         , processed_scene_mask, scene_mask, target_mask, obj_masks, rot_ids, labels, obstacle_ids, bbox
