33c60ddff3145a35f636f4c506cd2f0a42b8eb7e
Chrisantus Eze <ezechris30@gmail.com>
2024-06-05 08:33:11 -0500

    training models_heuristics using 35k dataset

--- trainer/train_new.py
+++ trainer/train_new.py
@@ -5,8 +5,8 @@ import copy
 # from policy.models_multi_task import Regressor, ResFCN
 # from policy.models_obstacle import Regressor, ResFCN
 # from policy.models_obstacle_attn import Regressor, ResFCN
-# from policy.models_obstacle_heuristics import Regressor, ResFCN
-from policy.models_obstacle_vit import Regressor, ResFCN
+from policy.models_obstacle_heuristics import Regressor, ResFCN
+# from policy.models_obstacle_vit import Regressor, ResFCN
 
 import torch
 import torch.optim as optim
