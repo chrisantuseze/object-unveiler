0f61287adb52b2087ff9e2632f13f48067b3eafe
Chrisantus Eze <ezechris30@gmail.com>
2024-08-02 15:54:48 -0500

    fixing errors that come with running act

--- eval_agent.py
+++ eval_agent.py
@@ -119,6 +119,108 @@ def run_episode_multi(policy: Policy, env: Environment, segmenter: ObjectSegment
     logging.info('--------')
     return episode_data, success_count, grp_count
 
+# multi output using attn
+def run_episode_act(policy: Policy, env: Environment, segmenter: ObjectSegmenter, rng, episode_seed, success_count, max_steps=15, train=True, grp_count=0):
+    env.seed(episode_seed)
+    obs = env.reset()
+
+    while not policy.is_state_init_valid(obs):
+        obs = env.reset()
+
+    episode_data = {'sr-1': 0,
+                    'sr-n': 0,
+                    'fails': 0,
+                    'attempts': 0,
+                    'collisions': 0,
+                    'objects_removed': 0,
+                    'objects_in_scene': len(obs['full_state']),
+                    'clutter_score': 0.0,
+                    'singulation_score': 0.0,
+                }
+    
+    processed_masks, pred_mask, raw_masks = segmenter.from_maskrcnn(obs['color'][1], dir=TEST_EPISODES_DIR)
+    cv2.imwrite(os.path.join(TEST_DIR, "initial_scene.png"), pred_mask)
+
+    # get a randomly picked target mask from the segmented image
+    target_mask, target_id = general_utils.get_target_mask(processed_masks, obs, rng)
+    cv2.imwrite(os.path.join(TEST_DIR, "initial_target_mask.png"), target_mask)
+    
+    i = 0
+    n_prev_masks = 0
+    count = 0
+
+    max_steps = 400
+    while episode_data['attempts'] < max_steps:
+        grp_count += 1
+        logging.info("Grasping count -", grp_count)
+
+        cv2.imwrite(os.path.join(TEST_DIR, "target_mask.png"), target_mask)
+        cv2.imwrite(os.path.join(TEST_DIR, "scene.png"), pred_mask)
+
+        state = policy.state_representation(obs)
+        action = policy.exploit_act(state, obs['color'][1], target_mask)
+
+        env_action3d = policy.action3d(action)
+        next_obs, grasp_info = env.step_act(env_action3d)
+
+        episode_data['attempts'] += 1
+        if grasp_info['collision']:
+            episode_data['collisions'] += 1
+
+        if grasp_info['stable'] and i ==0:
+            episode_data['sr-1'] += 1
+
+        if grasp_info['stable']:
+            episode_data['sr-n'] += 1
+            episode_data['objects_removed'] += 1
+
+        else:
+            episode_data['fails'] += 1
+
+        print(action)
+        print(grasp_info)
+        print('---------')
+
+        general_utils.delete_episodes_misc(TEST_EPISODES_DIR)
+
+        if policy.is_terminal(next_obs):
+            break
+
+        obs = copy.deepcopy(next_obs)
+
+        processed_masks, pred_mask, raw_masks = segmenter.from_maskrcnn(obs['color'][1], dir=TEST_EPISODES_DIR)
+        if len(processed_masks) == n_prev_masks:
+            count += 1
+
+        if count > 1:
+            logging.info("Robot is in an infinite loop")
+            break
+
+        target_id, target_mask = grasping.find_target(processed_masks, target_mask)
+        if target_id == -1:
+            if grasp_info['stable']:
+                logging.info("Target has been grasped!")
+                success_count += 1
+            else:
+                logging.info("Target could not be grasped. And it is no longer available in the scene.")
+
+            print('------------------------------------------')
+            break
+
+        ############# Calculating scores ##########
+        clutter_score = grasping.measure_clutter_segmentation(processed_masks)
+        print("clutter_score:", clutter_score)
+        episode_data['clutter_score'] += clutter_score
+
+        singulation_score = grasping.measure_singulation(target_id, processed_masks)
+        print("singulation_score:", singulation_score)
+        episode_data['singulation_score'] += singulation_score
+
+        n_prev_masks = len(processed_masks)
+
+    logging.info('--------')
+    return episode_data, success_count, grp_count
+
 # original
 def run_episode_old2(policy: Policy, env: Environment, segmenter: ObjectSegmenter, rng, episode_seed, success_count=0, max_steps=15, train=True):
     env.seed(episode_seed)
