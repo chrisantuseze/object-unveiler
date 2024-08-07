ea6da3003bd0c904479667dc0bd439b616806bb9
Chrisantus Eze <ezechris30@gmail.com>
2024-08-07 14:51:06 -0500

    reduced the frequence of saving the traj data

--- policy/policy.py
+++ policy/policy.py
@@ -484,7 +484,7 @@ class Policy:
 
         image_data = torch.from_numpy(all_cam_images / 255.0).float().to(self.device).unsqueeze(0)
         print("image_data.shape", image_data.shape)
-        image_data = torch.einsum('k h w c -> k c h w', image_data)
+        image_data = torch.einsum('b k h w c -> b k c h w', image_data)
 
         return image_data
 
