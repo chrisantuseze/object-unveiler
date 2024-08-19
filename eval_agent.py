487c61e285c858247883a4b876a7f7d20b58b93a
Chrisantus Eze <ezechris30@gmail.com>
2024-08-19 02:43:05 -0500

    update

--- eval_agent.py
+++ eval_agent.py
@@ -177,7 +177,7 @@ def run_episode_act(args, policy: Policy, env: Environment, segmenter: ObjectSeg
             if t % query_frequency == 0:
                 print("Getting fresh actions for timestep -", t, ", ", env.current_state)
                 actions = policy.exploit_act(state, obs)
-                # print("The actions gotten:", actions)
+                print("The actions gotten:", actions)
 
             if temporal_agg:
                 all_time_actions[[t], t:t+num_queries] = actions
