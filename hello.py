808b9cd8a4e9f33c796d26f6d73caa63cc7af002
Chrisantus Eze <ezechris30@gmail.com>
2024-08-19 11:35:54 -0500

    update

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ dir = ""
 # Specify the path to the folder containing the files you want to rename
 
 folder_path = "ppg-dataset/"
-id = 667
+id = 0
 
 
 folder_path = dir + folder_path
@@ -16,7 +16,7 @@ for filename in os.listdir(folder_path):
     old_name = os.path.join(folder_path, filename)
     
     arr = filename.split("_")
-    new_filename = arr[0] + "_" + str(id).zfill(5)
+    new_filename = arr[0] + "_" + str(id).zfill(6)
 
     # Rename the file
     os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
