f7f36dc8e491cb07811fc05fa2261a11ac3fe9e6
Chrisantus Eze <ezechris30@gmail.com>
2024-09-08 22:24:17 -0500

    update

--- act/utils_act.py
+++ act/utils_act.py
@@ -115,7 +115,7 @@ class ACTUnveilerDataset(torch.utils.data.Dataset):
         for data in trajectory_data:
             qpos, qvel, img = data
             joint_pos.append(qpos[:4]) # 4 is the number of joint in the FBHand NB: From self.joint_ids in environment.py
-            joints_vel.append(qvel[:4])
+            # joints_vel.append(qvel[:4])
             images.append(img)
         
         actions = data['actions'][:self.sequence_len]
