fe3025e5a5522795bc094e7fecbf389a6c18faee
Chrisantus Eze <ezechris30@gmail.com>
2024-10-25 15:05:39 -0500

    update

--- policy/policy.py
+++ policy/policy.py
@@ -87,7 +87,7 @@ class Policy:
         task_config = SIM_TASK_CONFIGS['sim_object_unveiler']
 
         ckpt_dir = "act/ckpt"
-        ckpt_name = f'policy_epoch_1000_seed_0.ckpt'
+        ckpt_name = f'policy_epoch_800_seed_0.ckpt'
         # ckpt_name = f'policy_best.ckpt'
         # ckpt_name = f'policy_last.ckpt'
         state_dim = 8
