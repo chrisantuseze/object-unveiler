a0ad8d4aac227995887e66155ea57873a4464c1a
Chrisantus Eze <ezechris30@gmail.com>
2024-10-25 15:00:37 -0500

    update

--- act/constants.py
+++ act/constants.py
@@ -32,7 +32,7 @@ SIM_TASK_CONFIGS = {
     },
 
     'sim_object_unveiler': {
-        'dataset_dir': DATA_DIR + '/ppg-dataset2', #'/working-ds1/ppg-dataset2',
+        'dataset_dir': DATA_DIR + '/working-ds1/ppg-dataset2',
         'num_episodes': 50,
         'episode_len': 3,
         # 'camera_names': ['top', 'front', 'heightmap', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
