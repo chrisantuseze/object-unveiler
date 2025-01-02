0f42cbb3a0c92bda4944d6d6f80b3a95a6d633f3
Chrisantus Eze <ezechris30@gmail.com>
2025-01-02 13:53:48 -0600

    update

--- playground.py
+++ playground.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import zipfile
 
-with zipfile.ZipFile("ppg-dataset-single.zip", 'r') as zip_ref:
+with zipfile.ZipFile("ppg-dataset2.zip", 'r') as zip_ref:
     zip_ref.extractall("ppg-dataset2")
