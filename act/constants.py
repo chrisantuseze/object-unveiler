b3c0bbe3ec066c6f00e3063c670376a73d8045d0
Chrisantus Eze <ezechris30@gmail.com>
2025-01-16 15:40:49 -0600

    training using 4 demonstrations

--- act/constants.py
+++ act/constants.py
@@ -32,7 +32,7 @@ SIM_TASK_CONFIGS = {
     },
 
     'sim_object_unveiler': {
-        'dataset_dir': DATA_DIR + '/ppg-dataset22',
+        'dataset_dir': DATA_DIR + '/ppg-dataset23',
         'num_episodes': 50,
         'episode_len': 1170, #from env
         'camera_names': ['top', 'front', 'heightmap', 'target']
