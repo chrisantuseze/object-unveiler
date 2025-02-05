9ab8752f91395e69eef04220011e0fc252384977
Chrisantus Eze <ezechris30@gmail.com>
2025-02-05 04:13:38 -0600

    update

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ for i, filename in enumerate(os.listdir(folder_path)):
     old_name = os.path.join(folder_path, filename)
     
     arr = filename.split("_")
-    new_filename = arr[0] + "_" + str(id).zfill(6)
+    new_filename = arr[0] + "_" + str(id).zfill(5)
 
     # Rename the file
     os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
