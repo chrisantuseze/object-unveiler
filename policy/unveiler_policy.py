0fcf2e5c8880ca82d8eb47dd34269b3b05bed62c
Chrisantus Eze <ezechris30@gmail.com>
2025-02-11 11:40:31 -0600

    update

--- policy/unveiler_policy.py
+++ policy/unveiler_policy.py
@@ -172,8 +172,8 @@ class SpatialTransformerPredictor(nn.Module):
         padding_mask_expanded = padding_masks.expand_as(logits)
         logits = logits.masked_fill_(padding_mask_expanded, float(-1e-6))
 
-        _, top_indices = torch.topk(logits, k=self.args.sequence_length, dim=1)
-        print("top indices", top_indices)
+        # _, top_indices = torch.topk(logits, k=self.args.sequence_length, dim=1)
+        # print("top indices", top_indices)
         
         # Sampling from the attention weights to get hard attention
         sampled_attention_weights = torch.zeros_like(logits)
