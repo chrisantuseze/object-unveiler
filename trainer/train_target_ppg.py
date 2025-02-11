423fb589533b754d7712277524d6daa5522d3ae2
Chrisantus Eze <ezechris30@gmail.com>
2025-02-11 12:24:59 -0600

    training target-ppg on cc

--- trainer/train_target_ppg.py
+++ trainer/train_target_ppg.py
@@ -43,16 +43,18 @@ def train_fcn_net(args):
     transition_dirs = os.listdir(args.dataset_dir)
     
     for file_ in transition_dirs:
-        # if not file_.startswith("episode"):
-        #     transition_dirs.remove(file_)
-        if not file_.startswith("transition"):
+        if not file_.startswith("episode"):
             transition_dirs.remove(file_)
+        # if not file_.startswith("transition"):
+        #     transition_dirs.remove(file_)
     
     # split data to training/validation
     random.seed(0)
     random.shuffle(transition_dirs)
 
     # transition_dirs = transition_dirs[:8000]
+
+    print(f'\nData from: {args.dataset_dir}; size: {len(transition_dirs)}\n')
 
     split_index = int(args.split_ratio * len(transition_dirs))
     train_ids = transition_dirs[:split_index]
