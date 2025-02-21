trying to fix exploding gradient issue

policy/policy.py
@@ -43,7 +43,7 @@ class Policy:
         self.bounds = np.array(params['env']['workspace']['bounds'])
 
         self.crop_size = 32
-        self.push_distance = 0.12 #0.15 # distance of the floating hand from the object to be grasped
+        self.push_distance = 0.15 #0.12 #0.15 # distance of the floating hand from the object to be grasped
         self.z = 0.08 # distance of the floating hand from the table (vertical distance)
 
         self.fcn = ResFCN(args).to(self.device)

trainer/train_target_ppg.py
@@ -46,7 +46,7 @@ def train_fcn_net(args):
         if not file_.startswith("episode"):
             transition_dirs.remove(file_)
 
-    # transition_dirs = transition_dirs[:10000]
+    transition_dirs = transition_dirs[:10000]
             
     # split data to training/validation
     random.seed(0)
@@ -77,7 +77,7 @@ def train_fcn_net(args):
     logging.info('{} training data, {} validation data'.format(len(train_ids), len(val_ids)))
 
     model = ResFCN(args).to(args.device)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    optimizer = optim.AdamW(model.parameters(), lr=args.lr, betas=(0.9, 0.95))
     
     criterion = nn.BCELoss(reduction='mean')
     lowest_loss = float('inf')