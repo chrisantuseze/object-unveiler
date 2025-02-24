8056109d1192f8e76523f33572469a5fea5ae88c
Chrisantus Eze <ezechris30@gmail.com>
2025-02-24 17:01:29 -0600

    testing act

--- playground.py
+++ playground.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import zipfile
 
-with zipfile.ZipFile("pc-ou-dataset2.zip", 'r') as zip_ref:
-    zip_ref.extractall("pc-ou-dataset2")
+with zipfile.ZipFile("ppg-dataset-act67-2.zip", 'r') as zip_ref:
+    zip_ref.extractall("ppg-dataset-act67-2")
 
 # import os
 # import shutil
