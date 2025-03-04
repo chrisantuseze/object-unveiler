844e62313effb71d42aca930d82fb84d84075669
Chrisantus Eze <ezechris30@gmail.com>
2025-03-04 13:26:58 -0600

    update

--- hello.py
+++ hello.py
@@ -5,29 +5,29 @@ import shutil
 import numpy as np
 
 
-# # # dir = "/Users/chrisantuseze/Research/robot-learning/datasets/"
-# dir = ""
-# # Specify the path to the folder containing the files you want to rename
+# # dir = "/Users/chrisantuseze/Research/robot-learning/datasets/"
+dir = ""
+# Specify the path to the folder containing the files you want to rename
 
-# # folder_path = "save/pc-ou-dataset-new/"
-# # folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/using-pointcloud/single-target-grasping/target-ppg-using-9-12-objects/ppg-ou-dataset-9-12"
-# folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/object-unveiler/save/pc-ou-dataset2/"
-# id = 0
+# folder_path = "save/pc-ou-dataset-new/"
+# folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/using-pointcloud/single-target-grasping/target-ppg-using-9-12-objects/ppg-ou-dataset-9-12"
+folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/object-unveiler/save/pc-ou-dataset-new2/"
+id = 2330
 
 
-# folder_path = dir + folder_path
-# # Loop through the files in the folder
-# for i, filename in enumerate(os.listdir(folder_path)):
-#     old_name = os.path.join(folder_path, filename)
+folder_path = dir + folder_path
+# Loop through the files in the folder
+for i, filename in enumerate(os.listdir(folder_path)):
+    old_name = os.path.join(folder_path, filename)
     
-#     arr = filename.split("_")
-#     new_filename = arr[0] + "_" + str(id).zfill(5)
+    arr = filename.split("_")
+    new_filename = arr[0] + "_" + str(id).zfill(5)
 
-#     # Rename the file
-#     os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
-#     id += 1
+    # Rename the file
+    os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
+    id += 1
 
-# print(id)
+print(id)
 
 def load_episode(dataset_dir, episode):
     # Ensure there's a valid image. If there's none, search through the timesteps
