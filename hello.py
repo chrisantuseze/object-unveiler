fed677e2ea4ddaf4b58e8d21c2ae0ef4b4b76db3
Chrisantus Eze <ezechris30@gmail.com>
2025-03-10 09:10:52 -0500

    update

--- hello.py
+++ hello.py
@@ -5,28 +5,28 @@ import shutil
 import numpy as np
 
 
-dir = ""
-# Specify the path to the folder containing the files you want to rename
+# dir = ""
+# # Specify the path to the folder containing the files you want to rename
 
-# folder_path = "save/pc-ou-dataset/"
-# folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/using-pointcloud/single-target-grasping/target-ppg-using-9-12-objects/ppg-ou-dataset-9-12"
-folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/object-unveiler/save/pc-ou-dataset2/"
-id = 0
+# # folder_path = "save/pc-ou-dataset/"
+# # folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/using-pointcloud/single-target-grasping/target-ppg-using-9-12-objects/ppg-ou-dataset-9-12"
+# folder_path = "/home/e_chrisantus/Projects/grasping_in_clutter/object-unveiler/save/pc-ou-dataset2/"
+# id = 0
 
 
-folder_path = dir + folder_path
-# Loop through the files in the folder
-for i, filename in enumerate(os.listdir(folder_path)):
-    old_name = os.path.join(folder_path, filename)
+# folder_path = dir + folder_path
+# # Loop through the files in the folder
+# for i, filename in enumerate(os.listdir(folder_path)):
+#     old_name = os.path.join(folder_path, filename)
     
-    arr = filename.split("_")
-    new_filename = arr[0] + "_" + str(id).zfill(5)
+#     arr = filename.split("_")
+#     new_filename = arr[0] + "_" + str(id).zfill(5)
 
-    # Rename the file
-    os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
-    id += 1
+#     # Rename the file
+#     os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
+#     id += 1
 
-print(id)
+# print(id)
 
 def load_episode(dataset_dir, episode):
     # Ensure there's a valid image. If there's none, search through the timesteps
@@ -48,41 +48,46 @@ def load_episode(dataset_dir, episode):
     return None
 
 def main():
-    dataset_dir = "save/pc-ou-dataset"
+    dataset_dir = "save/pc-ou-dataset2-51k"
     transition_dirs = os.listdir(dataset_dir)
     for file_ in transition_dirs:
         if not file_.startswith("episode"):
             transition_dirs.remove(file_)
 
-    count = 0
-    for episode in transition_dirs:
-        scene_mask, c_target_mask, c_object_masks, objects_to_remove, bboxes = load_episode(dataset_dir, episode)
+    episode_data = pickle.load(open(os.path.join(dataset_dir, transition_dirs[0]), 'rb'))
+    data = episode_data[0]
 
-        # if len(c_object_masks) == 0:
-        #     print(f"Deleting {episode}...")
-        #     episode_path = os.path.join(dataset_dir, episode)
-        #     os.remove(episode_path)  # Delete the file
+    print(data.keys())
 
-        # if len(objects_to_remove) == 1 and objects_to_remove[0] == -1:
-        #     print(f"Deleting {episode}...", objects_to_remove[0])
-        #     count += 1
-        #     episode_path = os.path.join(dataset_dir, episode)
-        #     os.remove(episode_path)  # Delete the file
+    # count = 0
+    # for episode in transition_dirs:
+    #     scene_mask, c_target_mask, c_object_masks, objects_to_remove, bboxes = load_episode(dataset_dir, episode)
 
-        # if 10 in objects_to_remove:
-        #     print(f"Deleting {episode}...", objects_to_remove)
-        #     episode_path = os.path.join(dataset_dir, episode)
-        #     os.remove(episode_path)  # Delete the file
-        #     count += 1
+    #     # if len(c_object_masks) == 0:
+    #     #     print(f"Deleting {episode}...")
+    #     #     episode_path = os.path.join(dataset_dir, episode)
+    #     #     os.remove(episode_path)  # Delete the file
 
-        for obj_mask in c_object_masks:
-            if isinstance(obj_mask, list):
-                print(f"Deleting {episode}...")
-                count += 1
-                episode_path = os.path.join(dataset_dir, episode)
-                os.remove(episode_path)  # Delete the file
-                break
+    #     # if len(objects_to_remove) == 1 and objects_to_remove[0] == -1:
+    #     #     print(f"Deleting {episode}...", objects_to_remove[0])
+    #     #     count += 1
+    #     #     episode_path = os.path.join(dataset_dir, episode)
+    #     #     os.remove(episode_path)  # Delete the file
 
-    print("Total count:", count)
+    #     # if 10 in objects_to_remove:
+    #     #     print(f"Deleting {episode}...", objects_to_remove)
+    #     #     episode_path = os.path.join(dataset_dir, episode)
+    #     #     os.remove(episode_path)  # Delete the file
+    #     #     count += 1
 
-# main()
+    #     for obj_mask in c_object_masks:
+    #         if isinstance(obj_mask, list):
+    #             print(f"Deleting {episode}...")
+    #             count += 1
+    #             episode_path = os.path.join(dataset_dir, episode)
+    #             os.remove(episode_path)  # Delete the file
+    #             break
+
+    # print("Total count:", count)
+
+main()
