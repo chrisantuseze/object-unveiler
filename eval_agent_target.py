7276ca061e26c8f87b3c63120db1716317f664db
Chrisantus Eze <ezechris30@gmail.com>
2025-04-10 12:26:54 -0500

    collecting results for target-ppg baseline

--- eval_agent_target.py
+++ eval_agent_target.py
@@ -380,7 +380,7 @@ def eval_agent(args):
         episode_seed = rng.randint(0, pow(2, 32) - 1)
         logging.info('Episode: {}, seed: {}'.format(i, episode_seed))
 
-        episode_data, success_count = run_episode_target(policy, env, segmenter, rng, episode_seed)
+        episode_data = run_episode_target(policy, env, segmenter, rng, episode_seed)
         eval_data.append(episode_data)
 
         sr_1 += episode_data['sr-1']
