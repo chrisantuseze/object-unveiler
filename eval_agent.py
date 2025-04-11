4e5fe193ac75e57c30a61335fe211a08ad372848
Chrisantus Eze <ezechris30@gmail.com>
2025-04-11 13:26:52 -0500

    collecting fresh results for unveiler 9-12 for partially occluded target

--- eval_agent.py
+++ eval_agent.py
@@ -441,7 +441,7 @@ def eval_agent(args):
         episode_seed = rng.randint(0, pow(2, 32) - 1)
         logging.info('Episode: {}, seed: {}'.format(i, episode_seed))
 
-        episode_data = run_episode_act(args, policy, env, segmenter, rng, episode_seed)
+        episode_data = run_episode_multi(args, policy, env, segmenter, rng, episode_seed)
         eval_data.append(episode_data)
 
         sr_1 += episode_data['sr-1']
