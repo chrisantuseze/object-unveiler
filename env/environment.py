5c8434795b5420cd740bc2a568e1a1168d6a8920
Chrisantus Eze <ezechris30@gmail.com>
2025-04-11 10:47:40 -0500

    collecting fresh results for encoder-only 6-9 for partially occluded target

--- env/environment.py
+++ env/environment.py
@@ -44,10 +44,10 @@ class Environment:
         self.pxl_size = params['env']['pixel_size']
         self.bounds = np.array(params['env']['workspace']['bounds'])
 
-        self.nr_objects = [1, 4]
+        # self.nr_objects = [1, 4]
 
         # self.nr_objects = [2, 6]
-        # self.nr_objects = [6, 9]
+        self.nr_objects = [6, 9]
         # self.nr_objects = [9, 12]
         # self.nr_objects = [12, 15]
 
