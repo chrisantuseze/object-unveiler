6f6845e0c441e4c129a1c749a7886bc80617e7a6
Chrisantus Eze <ezechris30@gmail.com>
2025-04-12 09:24:16 -0500

    collecting fresh results for encoder-only 9-12 for partially occluded target

--- env/environment.py
+++ env/environment.py
@@ -49,7 +49,6 @@ class Environment:
         # self.nr_objects = [2, 6]
         # self.nr_objects = [6, 9]
         self.nr_objects = [9, 12]
-        # self.nr_objects = [12, 15]
 
 
         # Setup cameras.
