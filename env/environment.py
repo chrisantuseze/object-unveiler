d73345f3cf44757b33fa393310398df12f1aef59
Chrisantus Eze <ezechris30@gmail.com>
2025-04-12 19:49:51 -0500

    collecting results for unveiler 2-6 for partially occluded target

--- env/environment.py
+++ env/environment.py
@@ -48,7 +48,7 @@ class Environment:
 
         self.nr_objects = [2, 6]
         # self.nr_objects = [6, 9]
-        self.nr_objects = [9, 12]
+        # self.nr_objects = [9, 12]
 
 
         # Setup cameras.
