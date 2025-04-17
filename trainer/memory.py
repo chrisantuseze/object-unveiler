04f4c2c2aad8e1bb9161860097cac8978f7b98dd
Chrisantus Eze <ezechris30@gmail.com>
2025-04-17 08:43:28 -0500

    training ppg baseline

--- trainer/memory.py
+++ trainer/memory.py
@@ -93,6 +93,7 @@ class ReplayBuffer:
         data = episode_data[0]
         heightmap = data['state']
         c_obstacle_mask = data['c_obstacle_mask']
+        # c_obstacle_mask = data['obstacle_mask']
         action = data['action']
 
         return heightmap, c_obstacle_mask, action
