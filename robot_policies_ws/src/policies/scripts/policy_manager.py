f83ab379bfd9453052ffd6d36e4a744f8b52b92c
chrisantuseze <ezechris30@gmail.com>
2025-04-22 16:12:48 -0500

    added more files and update for robot policies

--- robot_policies_ws/src/policies/scripts/policy_manager.py
+++ robot_policies_ws/src/policies/scripts/policy_manager.py
@@ -69,7 +69,7 @@ class PolicyManager:
         depth_image = self.bridge.imgmsg_to_cv2(obs_data.depth_image, desired_encoding="16UC1")
 
         target_mask = obs_data.target_mask
-        if target_mask is not None:
+        if target_mask is not None and target_mask.encoding:
             target_mask = self.bridge.imgmsg_to_cv2(target_mask, desired_encoding='mono8')
 
         # Convert from BGR (ROS standard) to RGB if needed
