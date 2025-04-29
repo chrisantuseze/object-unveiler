6d35e0b8e44e6cd0307ed04d7308a1c7218d7306
Chrisantus Eze <ezechris30@gmail.com>
2025-04-29 12:34:59 -0500

    comparing to gpt-4o

--- playground.py
+++ playground.py
@@ -86,15 +86,19 @@ def run_sre_policy():
 
         fig, ax = plt.subplots(2, 2)
         ax[0][0].imshow(scene_image)
+        ax[0][0].set_title("Scene - Color")
         ax[0][0].axis("off")
 
         ax[0][1].imshow(scene_mask)
+        ax[0][1].set_title("Scene - Grayscale")
         ax[0][1].axis("off")
 
         ax[1][0].imshow(c_target_mask)
+        ax[1][0].set_title("Target")
         ax[1][0].axis("off")
 
         ax[1][1].imshow(c_obstacle_mask)
+        ax[1][1].set_title("Obstacle")
         ax[1][1].axis("off")
         plt.show()
 
