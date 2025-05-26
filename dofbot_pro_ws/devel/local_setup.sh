209f416bc593d153cb47cfa10157ef7a0a4c7707
chrisantuseze <ezechris30@gmail.com>
2025-05-26 16:05:40 -0500

    updated robot files

--- dofbot_pro_ws/devel/local_setup.sh
+++ dofbot_pro_ws/devel/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/jetson/echris/object-unveiler/dofbot_pro_ws/devel}
+: ${_CATKIN_SETUP_DIR:=/home/e_chrisantus/Projects/object-unveiler/dofbot_pro_ws/devel}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
